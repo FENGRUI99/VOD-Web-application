16cdeff171d88d977f04dc15273e00226f000588
Fengrui99 <fengrui.cheng99@gmail.com>
2023-01-15 15:13:11 -0800

    Server commit

--- Content/Server.java
+++ Content/Server.java
@@ -1,7 +1,11 @@
 import java.io.*;
 import java.net.*;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.TextStyle;
 import java.util.Date;
+import java.util.Locale;
 
 public class Server {
     private DataOutputStream sOut = null;
@@ -12,75 +16,80 @@ public class Server {
     private static File f = null;
     final String CRLF = "\r\n";
     public void getServer() throws IOException {
+
         try {
             serverSocket = new ServerSocket(10008);
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        Socket clientSocket = null;
-        System.out.println("Waiting for connection.....");
-        try {
-            clientSocket = serverSocket.accept();
-        } catch (IOException e){
-            e.printStackTrace();
-        }
-        System.out.println ("Connection successful");
-        System.out.println ("Waiting for input.....");
+        while(true) {
+            Socket clientSocket = null;
+            System.out.println("Waiting for connection.....");
+            try {
+                clientSocket = serverSocket.accept();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Connection successful");
+            System.out.println("Waiting for input.....");
 
-        sOut = new DataOutputStream(clientSocket.getOutputStream());
-        sIn = new BufferedReader(new InputStreamReader( clientSocket.getInputStream()));
+            sOut = new DataOutputStream(clientSocket.getOutputStream());
+            sIn = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-        String inputLine;
+            String inputLine;
 
-        while ((inputLine = sIn.readLine()) != null)
-        {
-            System.out.println ("Server: " + inputLine);
-            //TODO
-            String[] info = inputLine.split(" ");
-            if (info[0].equals("GET")){
-                f = findFile(info[1]);
-                String fType = URLConnection.guessContentTypeFromName(f.getName());
-                Date date = new Date();
-                SimpleDateFormat dateFormat= new SimpleDateFormat("EEEE, dd-MM-yyyy hh:mm:ss");
-                String header = "HTTP/1.1 200 OK" +CRLF+
-                        "Content-Length:" + f.length() +CRLF+
-                        "Content-Type:" + fType +CRLF+
-                        "Date:" +  dateFormat.format(date) +" GMT"+CRLF+
-                        "Last-Modified:" + f.lastModified() +CRLF;
-                System.out.println(header);
-                try{
-                    FileInputStream fis = new FileInputStream(f);
-                    in = new DataInputStream(fis);
-                    byte[] bytes = new byte[1024];
-                    int length = 0;
-                    sOut.writeUTF(header);
-                    while ((length = fis.read(bytes, 0, bytes.length)) != -1) {
-                        sOut.write(bytes, 0, length);
-                        sOut.flush();
+            while ((inputLine = sIn.readLine()) != null){
+                System.out.println("Server: " + inputLine);
+                //TODO
+                String[] info = inputLine.split(" ");
+                if (info[0].equals("GET")) {
+//                    System.out.println(info);
+                    f = findFile(info[1]);
+                    if (!f.exists()) continue;
+
+                    String fType = URLConnection.guessContentTypeFromName(f.getName());
+                    Date date = new Date();
+                    SimpleDateFormat dateFormat1 = new SimpleDateFormat("EEEE, dd ");
+                    SimpleDateFormat dateFormat3 = new SimpleDateFormat(" yyyy hh:mm:ss");
+                    LocalDate localDate = LocalDate.now();
+                    Month dateFormat2 = localDate.getMonth();
+
+
+                    String header = "HTTP/1.1 200 OK" + CRLF +
+                            "Content-Length:" + f.length() + CRLF +
+                            "Content-Type:" + fType + CRLF +
+//                            "Content-Range:" + CRLF + start-end/size
+                            "Date:" + dateFormat1.format(date) + dateFormat2.getDisplayName(TextStyle.SHORT, Locale.ENGLISH) + dateFormat3.format(date) + " GMT" + CRLF +
+                            "Last-Modified:" + new Date(f.lastModified()) + CRLF + CRLF;
+//                System.out.println(header);
+                    try {
+                        FileInputStream fis = new FileInputStream(f);
+                        in = new DataInputStream(fis);
+                        byte[] bytes = new byte[1024000];
+                        int length = 0;
+                        sOut.writeUTF(header);
+                        while ((length = fis.read(bytes, 0, bytes.length)) != -1) {
+                            sOut.write(bytes, 0, length);
+                            sOut.flush();
+                        }
+                        System.out.println("successful");
+                    } catch (Exception e) {
+                        e.printStackTrace();
                     }
-                    System.out.println("successful");
-                } catch (Exception e){
-                    e.printStackTrace();
+                } else if (clientSocket.isClosed()) {
+                    break;
                 }
             }
-            else {
-                break;
-            }
 
-
+            sIn.close();
+            sOut.close();
+            in.close();
         }
-
-        sIn.close();
-        sOut.close();
-        in.close();
     }
 
     //TODO
     public File findFile(String path) {
-        byte[] bytes = new byte[1024];
         File file = new File("." + path);
-        System.out.println(file);
-        System.out.println(file.exists());
         return file;
     }
 
