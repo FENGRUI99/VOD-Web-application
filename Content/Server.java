2b470befc7253add8fc50b10c0fe4c6520e3896c
Fengrui99 <fengrui.cheng99@gmail.com>
2023-01-14 17:26:30 -0800

    second commit

--- Content/Server.java
+++ Content/Server.java
@@ -2,14 +2,16 @@ import java.io.*;
 import java.net.*;
 
 public class Server {
-    private BufferedOutputStream sOut = null;
-    private BufferedInputStream in = null;
+    private DataOutputStream sOut = null;
+    private DataInputStream in = null;
     private BufferedReader sIn = null;
     private ServerSocket serverSocket = null;
 
+    private static File f = null;
+
     public void getServer() throws IOException {
         try {
-            serverSocket = new ServerSocket(10007);
+            serverSocket = new ServerSocket(10008);
         } catch (Exception e){
             e.printStackTrace();
         }
@@ -23,40 +25,73 @@ public class Server {
         System.out.println ("Connection successful");
         System.out.println ("Waiting for input.....");
 
-        sOut = new BufferedOutputStream(clientSocket.getOutputStream());
+        sOut = new DataOutputStream(clientSocket.getOutputStream());
         sIn = new BufferedReader(new InputStreamReader( clientSocket.getInputStream()));
+        final String CRLF = "\r\n";
+        String response = "HTTP/1.1 200 OK" + CRLF+
+                "Content-Length:" + "240" + CRLF+
+                "Content-Type:" + "text/plain" + CRLF + CRLF;
+//        "Date:" + "" + CRLF+
+//                "Last-Modified:" + "" + CRLF;
+
         String inputLine;
 
         while ((inputLine = sIn.readLine()) != null)
         {
             System.out.println ("Server: " + inputLine);
-
             //TODO
-//            send file to client
+            String[] info = inputLine.split(" ");
+            if (info[0].equals("GET")){
+                f = findFile(info[1]);
+                try{
+                    FileInputStream fis = new FileInputStream(f);
+                    in = new DataInputStream(fis);
+                    byte[] bytes = new byte[128];
+                    int length = 0;
+//                    sOut.writeUTF(response);
+//                    String response_head = "HTTP/1.1 200 OK\r\n" +
+//                            "Content-type:text/html\r\n\r\n";
+//                    String response_body = "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">" +  // 解决乱码问题
+//                            "<style>body{font-family:\"黑体\"}</style>" +
+//                            "欢迎来到小蓝枣的博客<br>" +
+//                            "Welcome to xiaolanzao's blog!";
+                    sOut.writeUTF(response);
+//                    sOut.flush();
+                    System.out.println("successful");
+                    while ((length = fis.read(bytes, 0, bytes.length)) != -1) {
+                        sOut.write(bytes, 0, length);
+                        sOut.flush();
+                        System.out.println("successful");
+                    }
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+            else {
+                break;
+            }
+
+
         }
 
-
-
+        sIn.close();
+        sOut.close();
+        in.close();
     }
 
     //TODO
     public File findFile(String path) {
-        File file = new File("./video/" + path);
-        try{
-            FileInputStream fis = new FileInputStream(file);
-            in = new BufferedInputStream(fis);
-            while(in.available() > 0 ){
-                System.out.print(in.read());
-            }
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-
-        return null;
+        byte[] bytes = new byte[1024];
+        File file = new File("." + path);
+        System.out.println(file);
+        System.out.println(file.exists());
+        return file;
     }
 
     public static void main(String[] args) throws IOException{
         Server frame = new Server();
-        frame.findFile("test.png");
+        frame.getServer();
     }
+
+
 }
