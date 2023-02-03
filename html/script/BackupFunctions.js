6eb5c27fdd1bb9db3cdb3d5e98ee6db6c2682f03
ZWJRBEN <zwjr471278764@qq.com>
2023-02-02 22:05:25 -0800

    add video receiver code

--- html/script/BackupFunctions.js
+++ html/script/BackupFunctions.js
@@ -34,3 +34,17 @@
 //         .then(data => console.log("Success:", data))
 //         .catch(error => console.error("Error:", error));
 // }
+
+// response as a text from server:
+// fetch('http://172.16.7.10:10007/' + receivedTextData)
+//     .then(response => response.arrayBuffer())
+//     .then(arrayBuffer => {
+//         const base64 = btoa(
+//             new Uint8Array(arrayBuffer).reduce(
+//                 (data, byte) => data + String.fromCharCode(byte),
+//                 ""
+//             )
+//         );
+//         const img = document.getElementById("receivedImage");
+//         img.src = "data:image/png;base64," + base64;
+//     });
--- html/script/ShowResponse.js
+++ html/script/ShowResponse.js
@@ -2,14 +2,42 @@ const receivedTextData = document.getElementById("receivedTextData");
 receivedTextData.innerHTML = sessionStorage.getItem("textdata");
 
 fetch('http://172.16.7.10:10007/' + receivedTextData)
-    .then(response => response.arrayBuffer())
-    .then(arrayBuffer => {
-        const base64 = btoa(
-            new Uint8Array(arrayBuffer).reduce(
-                (data, byte) => data + String.fromCharCode(byte),
-                ""
-            )
-        );
-        const img = document.getElementById("receivedImage");
-        img.src = "data:image/png;base64," + base64;
+    .then(response => {
+        const contentType = response.headers.get("Content-Type");
+        if (contentType.includes("text/plain")) {
+            return response.text();
+        } else if (contentType.includes("image/jpeg") || contentType.includes("image/png")) {
+            return response.blob();
+        } else if (contentType.includes("audio/ogg")) {
+            return response.blob().then(blob => {
+                const audio = new Audio();
+                audio.src = URL.createObjectURL(blob);
+                audio.controls = true;
+                return audio;
+            });
+        } else if (contentType.includes("video/ogg")) {
+            return response.blob().then(blob => {
+                const video = document.createElement("video");
+                video.src = URL.createObjectURL(blob);
+                video.controls = true;
+                return video;
+            });
+        } else {
+            throw new Error("Unsupported content type: " + contentType);
+        }
+    })
+    .then(data => {
+        const receivedData = document.getElementById("receivedData");
+        if (data instanceof Blob) {
+            const image = new Image();
+            image.src = URL.createObjectURL(data);
+            receivedData.appendChild(image);
+        } else if (data instanceof Audio || data instanceof HTMLVideoElement) {
+            receivedData.appendChild(data);
+        } else {
+            receivedData.innerHTML = data;
+        }
+    })
+    .catch(error => {
+        console.error(error);
     });
