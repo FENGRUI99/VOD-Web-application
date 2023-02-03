3657d001aba6d36331a8c5f9e1fb9bedbe986dfa
ZWJRBEN <zwjr471278764@qq.com>
2023-02-02 16:26:34 -0800

    create a backup and delete comments

---
+++ html/script/BackupFunctions.js
@@ -0,0 +1,22 @@
+// open a web page:
+// var url = 'https://www.google.com/search?q=' + oInp.value;
+// window.open(url);
+
+// fetch with json:
+// fetch("http://172.16.7.10:10007", {
+//     method: "POST",
+//     headers:{
+//         "Content-Type": "application/json"
+//     },
+//     body: JSON.stringify(data)
+// })
+
+// fetch with text:
+// fetch('http://172.16.7.10:10007/', {
+//     method: "POST",
+//     headers:{
+//         "Content-Type": "text/plain"
+//     },
+//     body: oInp.value
+// })
+
--- html/script/SendText.js
+++ html/script/SendText.js
@@ -13,24 +13,6 @@ document.onkeydown = function () {
 
 
 function SendToServer() {
-    // var url = 'https://www.google.com/search?q=' + oInp.value;
-    // window.open(url);
-
-    // fetch("http://172.16.7.10:10007", {
-    //     method: "POST",
-    //     headers:{
-    //         "Content-Type": "application/json"
-    //     },
-    //     body: JSON.stringify(data)
-    // })
-
-    // fetch('http://172.16.7.10:10007/', {
-    //     method: "POST",
-    //     headers:{
-    //         "Content-Type": "text/plain"
-    //     },
-    //     body: oInp.value
-    // })
     fetch('http://172.16.7.10:10007/'+ oInp.value)
         .then(response => response.text())
         .then(data => console.log("Success:", data))
