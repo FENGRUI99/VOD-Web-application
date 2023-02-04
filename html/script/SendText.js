0bbfcc9d5c3c8a9bb9a528c0ae8d12be08779fed
ZWJRBEN <zwjr471278764@qq.com>
2023-02-03 19:01:22 -0800

    update html

--- html/script/SendText.js
+++ html/script/SendText.js
@@ -2,13 +2,25 @@ var oInp = document.getElementById('inp');
 var oBtn = document.getElementById('btn');
 
 oBtn.onclick = function () {
-    sessionStorage.setItem("textdata", oInp.value);
-    window.location.href = "showResponse.html";
+    if(oInp.value.startsWith("peer/add")) {
+        document.getElementById('peers').innerText = "Peers info: " + oInp.value;
+        fetch('http://172.16.7.10:8080/'+ oInp.value)
+        .catch(error => console.error("Error:", error));
+    } else{
+        sessionStorage.setItem("textdata", oInp.value);
+        window.location.href = "showResponse.html";
+    }
 }
 
 document.onkeydown = function () {
     if (event.keyCode == 13) {
-        sessionStorage.setItem("textdata", oInp.value);
-        window.location.href = "showResponse.html";
+        if(oInp.value.startsWith("peer/add")) {
+            document.getElementById('peers').innerText = "Peers info: " + oInp.value;
+            fetch('http://172.16.7.10:8080/'+ oInp.value)
+                .catch(error => console.error("Error:", error));
+        } else{
+            sessionStorage.setItem("textdata", oInp.value);
+            window.location.href = "showResponse.html";
+        }
     }
 }
