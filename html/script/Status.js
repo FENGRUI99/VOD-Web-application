b259448128eb2184ba6c7a7a97fb64065a800119
ZWJRBEN <zwjr471278764@qq.com>
2023-02-08 02:56:42 -0800

    edit status js

--- html/script/Status.js
+++ html/script/Status.js
@@ -3,26 +3,27 @@ const path = 'http://127.0.0.1:8080/';
 fetch(path + "status/request")
     .then(response => response.json())
     .then(data => {
-        console.log(data);
         const loadingBar = document.getElementById("loadingBar");
         const loadingPercentage = document.getElementById("loadingPercentage");
-        const table = document.getElementById("dataTable");
+        // const table = document.getElementById("dataTable");
 
-        for (let i = 0; i < data.length; i++) {
-            const item = data[i];
-            const row = table.insertRow();
+        const fileName = document.getElementById("fileName");
+        fileName.innerHTML = "File Name: " + data.fileName;
 
-            const fileNameCell = row.insertCell();
-            fileNameCell.innerHTML = item.fileName;
+        const start = document.getElementById("start");
+        start.innerHTML = "Start: " + data.start + " bytes";
 
-            const fileSizeCell = row.insertCell();
-            fileSizeCell.innerHTML = item.fileSize;
+        const fileSize = document.getElementById("fileSize");
+        fileSize.innerHTML = "File Size: " + data.fileSize + " bytes";
 
-            const bitRateCell = row.insertCell();
-            bitRateCell.innerHTML = item.bitRate;
-        }
+        const bitRate = document.getElementById("bitRate");
+        bitRate.innerHTML = "Bit Rate: " + data.bitRate + " bps";
 
-        const percentage = data.progress;
+        const percentage = (data.progress * 100).toFixed(2);
+
+        const per = document.getElementById("per");
+        per.innerHTML = "Percentage: " + percentage + " %";
+
         loadingBar.style.width = `${percentage}%`;
         loadingPercentage.innerHTML = `${percentage}%`;
     })
