6e52d3489713995ad447da19f214ad458ed6b08b
ZWJRBEN <zwjr471278764@qq.com>
2023-02-08 02:10:56 -0800

    edit status js

--- html/script/Status.js
+++ html/script/Status.js
@@ -3,11 +3,13 @@ const path = 'http://127.0.0.1:8080/';
 fetch(path + "status/request")
     .then(response => response.json())
     .then(data => {
-        //TODO: finish static loading bar
+        console.log(data);
+        const loadingBar = document.getElementById("loadingBar");
+        const loadingPercentage = document.getElementById("loadingPercentage");
+        const table = document.getElementById("dataTable");
 
-        const table = document.getElementById('dataTable');
-
-        data.forEach(item => {
+        for (let i = 0; i < data.length; i++) {
+            const item = data[i];
             const row = table.insertRow();
 
             const fileNameCell = row.insertCell();
@@ -18,9 +20,18 @@ fetch(path + "status/request")
 
             const bitRateCell = row.insertCell();
             bitRateCell.innerHTML = item.bitRate;
-        });
+        }
+
+        const percentage = data.progress;
+        loadingBar.style.width = `${percentage}%`;
+        loadingPercentage.innerHTML = `${percentage}%`;
     })
     .catch(error => {
         console.error(error);
     });
+
+
+
+
+
 
