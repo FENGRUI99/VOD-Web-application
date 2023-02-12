7b89f857803aeb76dc0d8e4323484ebfcad616f2
Fengrui99 <fengrui.cheng99@gmail.com>
2023-02-12 15:51:00 -0800

    2.5.2 config rate

--- Router.java
+++ Router.java
@@ -1,11 +1,11 @@
+import com.alibaba.fastjson.JSON;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
-import java.util.UUID;
+import java.util.*;
+
 public class Router {
     static String frontEndPort;
     static String backEndPort;
@@ -14,6 +14,7 @@ public class Router {
     static String peerCount;
     static String uuid;
     static String content;
+    static HashMap<String, String> peerInfo; // peerName -> sequence
     public Router(){
         File configFile = new File("node.config");
         // Create a Properties object
@@ -38,6 +39,14 @@ public class Router {
         if(uuid.length() != 36){
             uuid = UUID.randomUUID().toString();
         }
+        peerInfo = new HashMap<>();
+    }
+    public static void receive(){
+
+    }
+
+    public static void send(){
+
     }
 
     public static void main(String[] args) {
@@ -54,7 +63,7 @@ public class Router {
         System.out.println(Router.content);
     }
     // change the information in node.config
-    private void setConfig(String fileName, String uuid, String name, int frontEndPort, int backEndPort, String contentDir, int peerCount, List<String> peers){
+    private static void setConfig(String fileName, String uuid, String name, int frontEndPort, int backEndPort, String contentDir, int peerCount, List<String> peers){
         File configFile = new File(fileName);
         Properties configProperties = new Properties();
         // Modify the properties
