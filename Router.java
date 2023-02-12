68054f312422f1752600478ca98d9639fe167441
ZongjinLi <ZongjinLi1231@outlook.com>
2023-02-11 21:17:19 -0800

    3.0.2

--- Router.java
+++ Router.java
@@ -1,22 +1,77 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Properties;
+import java.util.UUID;
 public class Router {
-    int frontEndPort;
-    int backEndPort;
-    String name;
-    List<String> peers;
-    int peerCount;
-    String uuid;
-    String content;
+    static String frontEndPort;
+    static String backEndPort;
+    static String name;
+    static List<String> peers;
+    static String peerCount;
+    static String uuid;
+    static String content;
     public Router(){
+        File configFile = new File("node.config");
+        // Create a Properties object
+        Properties configProperties = new Properties();
+        //Read the properties file
+        try (FileInputStream input = new FileInputStream(configFile)) {
+            configProperties.load(input);
+        } catch (IOException e) {
+            System.out.println("Error reading config file: " + e.getMessage());
+        }
 
-    }
-
-    public void readConfig(){
-
+        frontEndPort = configProperties.getProperty("frontend_port");
+        backEndPort = configProperties.getProperty("backend_port");
+        name = configProperties.getProperty("name");
+        peerCount = configProperties.getProperty("peer_count");
+        content = configProperties.getProperty("content_dir");
+        peers = new ArrayList<String>();
+        for(int i = 0; i < Integer.valueOf(peerCount); i++){
+            peers.add(configProperties.getProperty("peer_" + i));
+        }
+        uuid = configProperties.getProperty("uuid");
+        if(uuid.length() != 36){
+            uuid = UUID.randomUUID().toString();
+        }
     }
 
     public static void main(String[] args) {
-
+        Router r = new Router();
+        System.out.println(Router.frontEndPort);
+        System.out.println(Router.backEndPort);
+        System.out.println(Router.name);
+        System.out.println(Router.peers.size());
+        for(int i = 0; i < peers.size(); i++){
+            System.out.println(Router.peers.get(i));
+        }
+        System.out.println(Router.peerCount);
+        System.out.println(Router.uuid);
+        System.out.println(Router.content);
+    }
+    // change the information in node.config
+    private void setConfig(String fileName, String uuid, String name, int frontEndPort, int backEndPort, String contentDir, int peerCount, List<String> peers){
+        File configFile = new File(fileName);
+        Properties configProperties = new Properties();
+        // Modify the properties
+        configProperties.setProperty("uuid", uuid);
+        configProperties.setProperty("name", name);
+        configProperties.setProperty("frontend_port", ""+frontEndPort);
+        configProperties.setProperty("backend_port", ""+backEndPort);
+        configProperties.setProperty("content_dir", contentDir);
+        configProperties.setProperty("peer_count", ""+peerCount);
+        for(int i = 0; i < peerCount; i++){
+            configProperties.setProperty("peer_"+i, peers.get(i));
+        }
+        // Write the properties back to the file
+        try (FileOutputStream output = new FileOutputStream(configFile)) {
+            configProperties.store(output, null);
+        } catch (IOException e) {
+            System.out.println("Error writing config file: " + e.getMessage());
+        }
     }
 }
--- testConfig.java
+++ testConfig.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -6,22 +7,10 @@ import java.util.Properties;
 public class testConfig {
     public static void main(String[] args) {
         // Define the file path
-        String configFile = "node.config";
-
-        // Create a Properties object
+        File configFile = new File("node.config");
         Properties configProperties = new Properties();
-
-        // Read the properties file
-//        try (FileInputStream input = new FileInputStream(configFile)) {
-//            configProperties.load(input);
-//        } catch (IOException e) {
-//            System.out.println("Error reading config file: " + e.getMessage());
-//        }
-//        String property1 = configProperties.getProperty("property1");
-//        System.out.println(property1);
-
         // Modify the properties
-        configProperties.setProperty("uuid", "f94fc272-5611-4a61-8b27-de7fe233797f");
+        configProperties.setProperty("uuid", "123ee632-56b5-4a15-88ef-7bd3b7081141");
         configProperties.setProperty("name", "node1");
         configProperties.setProperty("frontend_port", "18345");
         configProperties.setProperty("backend_port", "18346");
@@ -29,7 +18,6 @@ public class testConfig {
         configProperties.setProperty("peer_count", "2");
         configProperties.setProperty("peer_0", "24f22a83-16f4-4bd5-af63-b5c6e979dbb,pi.ece.cmu.edu,18345,18346,10");
         configProperties.setProperty("peer_1", "3d2f4e34-6d21-4dda-aa78-796e3507903c,mu.ece.cmu.edu,18345,18346,20");
-
         // Write the properties back to the file
         try (FileOutputStream output = new FileOutputStream(configFile)) {
             configProperties.store(output, null);
