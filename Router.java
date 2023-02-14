4c7bb2866fdc8860d2f57084a43a35950f8894b2
Fengrui99 <fengrui.cheng99@gmail.com>
2023-02-13 22:28:06 -0800

    2.5.3 routerMap

--- Router.java
+++ Router.java
@@ -1,13 +1,10 @@
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
+
+import java.awt.*;
+import java.io.*;
+import java.net.*;
 import java.util.*;
+import java.util.List;
 
 public class Router {
     String frontEndPort;
@@ -20,8 +17,8 @@ public class Router {
     int seq;
     HashMap<String, Integer> peerSeq; // peerName -> sequence
     JSONObject routerMap;
-    public Router(){
-        File configFile = new File("node.config");
+    public Router(String configName){
+        File configFile = new File(configName);
         // Create a Properties object
         Properties configProperties = new Properties();
         //Read the properties file
@@ -54,12 +51,11 @@ public class Router {
         }
         routerMap.put(uuid, localMap);
     }
-
     public void start() throws Exception {
         DatagramSocket dsock = new DatagramSocket(Integer.parseInt(backEndPort));
         byte[] sendArr = new byte[2048];
         DatagramPacket dpack = new DatagramPacket(sendArr, sendArr.length);
-        send(uuid, seq);
+        send(this.uuid, seq);
         while(true){
             dsock.receive(dpack);
             // 0:35 uuid
@@ -72,7 +68,7 @@ public class Router {
             JSONObject peerMap = JSONObject.parseObject(new String(recArr, 68, recArr.length - 68).trim());
             if (sequence > peerSeq.getOrDefault(uuid, -1)){
                 peerSeq.put(uuid, sequence);
-                routerMap.put(uuid, peerMap);
+                routerMap.put(uuid, peerMap.get(uuid));
                 send(uuid, sequence);
             }
 
@@ -83,13 +79,11 @@ public class Router {
             System.out.println(routerMap.toJSONString());
         }
     }
-
     //发送自身路由表 或 转发peers路由表
     public void send(String id, int sequence) throws Exception{
         DatagramSocket dsock = new DatagramSocket();
         DatagramPacket dpack;
         byte[] sendArr = routerMap.toJSONString().getBytes();
-        //System.out.println("44444444:" + routerMap.toJSONString());
 
         if(id == uuid){
             byte[] message = new byte[68+sendArr.length];
@@ -113,12 +107,10 @@ public class Router {
         }
 
         dsock.close();
-        //dpack.close();
-    }
 
+    }
     public static void main(String[] args) throws Exception {
-        Router router = new Router();
-        System.out.println(router.routerMap.get("bbbee632-56b5-4a15-88ef-7bd3b7081141"));
+        Router router = new Router("routerConfig/" + args[0]);
         router.start();
     }
     // change the information in node.config
@@ -141,5 +133,81 @@ public class Router {
         } catch (IOException e) {
             System.out.println("Error writing config file: " + e.getMessage());
         }
+    }
+}
+
+class Asker extends Thread{
+    List<String> peers;
+    String uuid;
+    int seq;
+    JSONObject routerMap;
+    HashMap<String, Integer> peerCount;
+    HashMap<String, String> peerDistance;
+    public Asker(List<String> peers, String uuid, int seq, JSONObject routerMap){
+        this.peers = peers;
+        this.uuid = uuid;
+        this.seq = seq;
+        this.routerMap = routerMap;
+        for (String peer : peers){
+            String[] tmp = peer.split(",");
+            peerCount.put(tmp[0], 0);
+            peerDistance.put(tmp[0], tmp[4]);
+        }
+    }
+
+    public void startThread() throws Exception{
+        DatagramSocket dsock = new DatagramSocket();
+        DatagramPacket dpack;
+        dsock.setSoTimeout(1000);
+        while (true){
+            long start = System.currentTimeMillis();
+            // do something
+            for (String peer : peers){
+                String[] tmp = peer.split(",");
+                byte[] header = (uuid + "-1").getBytes();
+                byte[] content = "alive".getBytes();
+                byte[] sendArr = new byte[header.length + content.length];
+                System.arraycopy(header, 0, sendArr, 0, header.length);
+                System.arraycopy(content, 0, sendArr, header.length, content.length);
+                dpack = new DatagramPacket(sendArr, sendArr.length, InetAddress.getByName(tmp[1]), Integer.valueOf(tmp[3]));
+                dsock.send(dpack);
+            }
+//            synchronized
+            while (true){
+                try{
+                    byte[] recArr = new byte[2048];
+                    dpack = new DatagramPacket(recArr, recArr.length);
+                    dsock.receive(dpack);
+                } catch (SocketTimeoutException e){
+                    //TODO 怎么写，既要考虑node下线，也要考虑node上线
+                    JSONObject localMap = routerMap.getJSONObject(uuid); //TODO 这句可能有问题
+                    for (String id : peerCount.keySet()){
+                        if (peerCount.get(id) <= 0 && localMap.containsKey(id)){
+                            localMap.remove(id);
+                        }
+                        else if (peerCount.get(id) > 0 && !localMap.containsKey(id)){
+                            localMap.put(id, peerDistance.get(id));
+                        }
+                    }
+                    routerMap.put(uuid, localMap);
+                    break;
+                }
+                byte[] recArr = dpack.getData();
+                String id = new String(recArr, 0, 36);
+                int count = peerCount.get(id);
+                peerCount.put(id, count - 1);
+            }
+
+            long end = System.currentTimeMillis();
+            try {
+                sleep(10 - (end - start) / 1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+    @Override
+    public void run() {
+
     }
 }
